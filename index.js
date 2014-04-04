015956b8b32cba6da78168e884337237d530cfe7
Alexander Surma <surma@surmair.de>
2014-04-04 16:43:02 +0000

    Split JS and use Grunt to reunify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var deployd = require('deployd');
-var s3 = require('s3-bucket-resource');
+require('s3-bucket-resource');
 var fs = require('fs');
 
 var s3conf = {
@@ -12,30 +12,30 @@ var s3conf = {
 };
 
 var server = deployd({
-  port: process.env.PORT || 5000,
-  env: process.env.ENV || 'production',
-  db: {
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: parseInt(process.env.DB_PORT) || 27017,
-    name: process.env.DB_NAME || 'matchboxarchive',
-    credentials: {
-      username: process.env.DB_USER || '',
-      password: process.env.DB_PASS || ''
+    port: process.env.PORT || 5000,
+    env: process.env.ENV || 'production',
+    db: {
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: parseInt(process.env.DB_PORT, 10) || 27017,
+        name: process.env.DB_NAME || 'matchboxarchive',
+        credentials: {
+            username: process.env.DB_USER || '',
+            password: process.env.DB_PASS || ''
+        }
     }
-  }
 });
 
 fs.writeFile("resources/bucket/config.json", JSON.stringify(s3conf), function() {
-	server.listen();
+    server.listen();
 
-	server.on('listening', function() {
-	  console.log("Server is listening");
-	});
+    server.on('listening', function() {
+        console.log("Server is listening");
+    });
 
-	server.on('error', function(err) {
-	  console.error(err);
-	  process.nextTick(function() { 
-	    process.exit();
-	  });
-	});
-}); 
+    server.on('error', function(err) {
+        console.error(err);
+        process.nextTick(function() {
+            process.exit();
+        });
+    });
+});
---
+++ src/js/searchctrl.js
@@ -0,0 +1,62 @@
+window.angular.module('matchboxarchive')
+.controller('searchctrl', ['$scope', '$interval', '$location', 'matchboxService', 'rolloutService', 'userService', 'CONFIG', function($scope, $interval, $location, matchboxService, rolloutService, userService, CONFIG) {
+    $scope.results = [];
+    $scope.page = 0;
+    $scope.hasMore = true;
+
+    $scope.isLoggedIn = function() {
+        return userService.isLoggedIn;
+    };
+
+    $scope.query = {
+        'visible': true,
+    };
+
+    $interval(function poll() {
+        if(!$scope.hasMore) {
+            return;
+        }
+
+        var offset = document.querySelector('.load-trigger').getClientRects()[0].top - window.innerHeight;
+        if(offset < 10) {
+            var pagedQuery = $scope.query;
+            pagedQuery['$skip'] = $scope.page*CONFIG.infiniteScrollLoad;
+            pagedQuery['$limit'] = CONFIG.infiniteScrollLoad;
+            matchboxService.query(pagedQuery).then(function(data) {
+                $scope.page += 1;
+                $scope.hasMore = data.data.length == CONFIG.infiniteScrollLoad;
+                for(var i in data.data) {
+                    $scope.results.push(data.data[i]);
+                }
+            })
+            return;
+        }
+    }, CONFIG.infiniteScrollPoll);
+
+    $scope.queryData = {};
+
+    $scope.newSearch = function() {
+        $scope.query = {};
+        if(!($scope.isLoggedIn() && ($scope.queryData.showHidden))) {
+            $scope.query['visible'] = true;
+        }
+        $scope.page = 0;
+        $scope.hasMore = true;
+        $scope.results = [];
+    };
+
+    $scope.showDetails = function(imgId) {
+        rolloutService.rollOut();
+        $location.path('/details/'+imgId);
+    };
+
+    $scope.closeDrawer = function() {
+        rolloutService.rollIn();
+        $location.path('/');
+    };
+
+    $scope.newMatchbox = function() {
+        rolloutService.rollOut();
+        $location.path('/upload');
+    }
+}]);
