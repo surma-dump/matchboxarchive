d24053c05e001fcedcafe2760901c96cf48b2525
Friedrich Wessel @Littlefoot <wessel@asdf-systems.de>
2015-03-01 21:00:25 +0100

    add dump document

---
+++ static/helps_and_stuff.js
@@ -0,0 +1,10 @@
+/* usefull links: 
+https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+
+css animations: 
+http://webdesign.tutsplus.com/tutorials/a-beginners-introduction-to-css-animation--cms-21068
+
+http://angular-ui.github.io/bootstrap/#/getting_started
+https://docs.angularjs.org/tutorial/step_07
+https://docs.angularjs.org/api/ng/directive/ngStyle
+*/
